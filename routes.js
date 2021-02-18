7f9c274b5bb665df46d36c777511a8d80dd878c6
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-02-18 16:23:09 +0700

    Bagus [Dev] create file routes

---
+++ routes.js
@@ -0,0 +1,9 @@
+//file ini fungsinya membuat semacam navigasi
+'use strict'
+
+module.exports = function (app) {
+    var jsonku = require("./controller");
+
+    app.route('/')
+        .get(jsonku.index);
+}
