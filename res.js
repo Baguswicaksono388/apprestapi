c1d167f034b9ab7fd90adc7149d56e10066195ae
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-02-18 16:06:11 +0700

    Bagus [Dev] Menambahkan kode res

---
+++ res.js
@@ -0,0 +1,11 @@
+'use strict' //ini fungsinya untuk mengetatkan variabel yang ada di js
+
+exports.ok = function (values, res) {
+    var data = {
+        'status': 200,
+        'values' : values
+    }
+
+    res.json(data);
+    res.end();
+}
