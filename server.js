a6d70211280a806a8e905f3063051bf7612b2362
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-02-18 16:36:45 +0700

    Bagus [Dev] setting routes at server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express(); //function to call expressjs
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//call routes
+var routes = require('./routes');
+routes(app); //call function app at routes
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
