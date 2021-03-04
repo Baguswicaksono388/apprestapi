32ee90fecc242c6e06a6ef23e7c1853cc763ea74
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-03-04 15:50:11 +0700

    Bagus [Dev] Create model Registration

---
+++ models/registration.models.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+    const Registration = sequelize.define("user", {
+        username: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        },
+        role_id: {
+            type: Sequelize.TINYINT
+        }
+    }, {
+        // Mengunci nama table
+        freezeTableName: true
+    });
+
+    return Registration;
+}
