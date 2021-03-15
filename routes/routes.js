cd3575d3e52ae439537f9773882c199ccd4794f8
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-03-15 11:12:49 +0700

    Bagus [Dev] Adding syntax API for delete-user, show-user, get-practice-study, & get-soal-latihan-sd

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@
 module.exports = app => {
     var posts = require("../controller/post.controller");
     var registration = require("../controller/registration.controller");
+    var practice_study = require("../controller/practice_study.controller");
     // let router = require("express").Router();
     // // // Create a new Post
     // router.post("/", posts.create);
@@ -14,4 +15,9 @@ module.exports = app => {
 
     app.route('/registration').post(registration.registration);
     app.route('/login').post(registration.login);
+    app.route('/delete-user').post(registration.deleteUser);
+    app.route('/show-user').get(registration.showUser);
+
+    app.route('/api/get-practice-study').post(practice_study.findAll);
+    app.route('/api/get-soal-latihan-sd').post(practice_study.getSoalLatihanSD);
 }
