b14d9bea8689ae339848d66386289b523a8b7e7a
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-03-15 11:04:10 +0700

    Bagus [Dev] Adding syntax role on index models

--- models/index.js
+++ models/index.js
@@ -25,5 +25,6 @@ db.sequelize = sequelize;
 
 db.post = require("./post.models.js")(sequelize, Sequilize);
 db.registration = require("./registration.models")(sequelize, Sequelize);
+db.role = require("./role.models")(sequelize, Sequelize);
 
 module.exports = db;
