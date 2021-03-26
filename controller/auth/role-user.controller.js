51b0a8d61b0bb0621139a78c590e9665a5ac4e4e
Bagus <sgiapi1@gmail.com>
2021-03-26 14:04:46 +0700

    Bagus [Dev] Membuat relasi untuk roles user

--- controller/auth/role-user.controller.js
+++ controller/auth/role-user.controller.js
@@ -1,5 +1,6 @@
 const db = require('../../models/index-auth');
 const RoleUser = db.role_user;
+const HasModelRoles = db.has_model_roles;
 
 exports.createRoleUser = (req, res) => {
     const post = {
@@ -25,4 +26,29 @@ exports.createRoleUser = (req, res) => {
             })
         })
     }
+}
+
+exports.roleUser = (req, res) => {
+    RoleUser.hasMany(HasModelRoles, { foreignKey: 'role_id'});
+    HasModelRoles.belongsTo(RoleUser, { foreignKey: 'role_id'});
+    HasModelRoles.findOne({
+        where: {
+            model_id: 2
+        },
+        include: [{
+            model: RoleUser,
+            where: {
+                url: '/bismillah/oke/oce'
+            }
+        }]
+    }).then(data => {
+        res.status(200).send({
+            message: "Success",
+            data: data
+        })
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        })
+    })
 }
