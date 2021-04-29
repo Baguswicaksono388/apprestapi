4d39ed11741c6e9309a8d60e32280d92dc813d4a
Bagus <sgiapi1@gmail.com>
2021-04-29 11:56:11 +0700

    Bagus [Dev] configuration env DB

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,12 @@
 // configurasi untuk sequilize
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "res_api",
+    HOST: process.env.HOST_API,
+    USER: process.env.USER_API,
+    PASSWORD: process.env.PASSWORD_API,
+    DB: process.env.DB_API,
     dialect: "mysql", //ini akan memakai query nya dari siapa ?
     pool: { //ini seperti request time out / time limit misal ada error
         max: 5,
